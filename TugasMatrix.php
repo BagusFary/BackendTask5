7cc0ecc8a18863d2726aecd820243551c4a1a77f
BagusFary <bagusfaryananta@gmail.com>
2023-09-25 22:46:55 +0700

    Commit

--- TugasMatrix.php
+++ TugasMatrix.php
@@ -14,11 +14,43 @@ $matriks2 = [
 
 // Perulangan Matriks
 foreach($matriks2 as $key => $val1){
-    $matriks = $matriks1[$key];
-    
+    $row = 0;
+    foreach($matriks1 as $val2){
+        $kali = $val1 * $val2;
+        $row = $row + $kali;
+    }
+
+    echo "<p>$row<p>";
+
 }
 
+echo "<hr>";
 
+$number = [
+    3, 2, 1
+];
+
+$star = [
+    "*", "*", "*", "*", "*",
+];
+
+for ($i = 0; $i < 5; $i++) {
+    if ($i == 0) foreach ($star as $data) {
+        echo $data;
+    } 
+
+    if ($i !== 0){
+        echo "*";
+        foreach ($number as $value) {
+            echo "$value";
+        }
+        array_shift($number);
+    } 
+
+    echo "<br>";
+
+    array_pop($star);
+}
 
 
 ?>
