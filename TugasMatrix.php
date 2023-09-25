5fe3e56f06ec916e54a3c33c52d5e1e6ff1d9881
BagusFary <bagusfaryananta@gmail.com>
2023-09-25 15:55:46 +0700

    first commit

---
+++ TugasMatrix.php
@@ -0,0 +1,24 @@
+<?php
+
+$matriks1 = [
+    5,5,5,5,5,
+];
+
+$matriks2 = [
+    6,
+    6,
+    6,
+    6,
+    6
+];
+
+// Perulangan Matriks
+foreach($matriks2 as $key => $val1){
+    $matriks = $matriks1[$key];
+    
+}
+
+
+
+
+?>
